ae2080c06044d48c5644c08af7057005acfd9854
Ilia (Elias) Motornyi <elijah.mot@gmail.com>
2023-09-14 23:41:11 +0300

    CMSIS-DSP try #1

--- Core/Src/app.cpp
+++ Core/Src/app.cpp
@@ -2,6 +2,7 @@
 #include "main.h"
 #include "stm32746g_discovery_lcd.h"
 #include "MAX30100.h"
+#include "dsp/transform_functions.h"
 
 static void LCD_Config() {
     BSP_SDRAM_Init();
@@ -20,16 +21,22 @@ static void LCD_Config() {
     BSP_LCD_SetTransparency(0, 255);
 }
 
-const int sampleCapacity = 480;
+const int sampleCapacity = 512;
 const int graphTop = 60;
 const int graphBottom = 272;
 const int graphHeight = graphBottom - graphTop;
 
 static MAX30100 sensor;
 
+static uint16_t sampleBufferIr[sampleCapacity];
+static uint16_t sampleBufferRed[sampleCapacity];
+static uint16_t fftBufferIr[sampleCapacity];
+
 void drawGraph(const uint16_t samples[sampleCapacity], uint32_t color);
 
 void clearGraph();
+
+void performFFT();
 
 void setupSensor() {
 
@@ -59,7 +66,7 @@ void setupSensor() {
 void drawGraph(const uint16_t samples[sampleCapacity], uint32_t color) {
     int topVal = samples[0];
     int bottomVal = topVal;
-    for (int i = 1; i < sampleCapacity; ++i) {
+    for (int i = 1; i < 480; ++i) {
         auto v = samples[i];
         if (v > topVal) topVal = v;
         if (v < bottomVal) bottomVal = v;
@@ -72,7 +79,7 @@ void drawGraph(const uint16_t samples[sampleCapacity], uint32_t color) {
         topVal += d;
         bottomVal -= d;
     }
-    for (int i = 0; i < sampleCapacity; ++i) {
+    for (int i = 0; i < 480; ++i) {
         auto v = samples[i];
         int y = graphBottom - graphHeight * (v - bottomVal) / (topVal - bottomVal);
         BSP_LCD_DrawPixel(i, y, color);
@@ -81,7 +88,7 @@ void drawGraph(const uint16_t samples[sampleCapacity], uint32_t color) {
 
 void clearGraph() {
     BSP_LCD_SetTextColor(LCD_COLOR_DARKGREEN);
-    BSP_LCD_FillRect(0, graphTop, sampleCapacity, graphHeight);
+    BSP_LCD_FillRect(0, graphTop, 480, graphHeight);
 }
 
 _Noreturn void App_Run(void) {
@@ -91,8 +98,6 @@ _Noreturn void App_Run(void) {
     BSP_LCD_DisplayStringAtLine(0, (uint8_t *) "MAX30100 Demo");
     BSP_LCD_SetFont(&Font24);
     setupSensor();
-    static uint16_t sampleBufferIr[sampleCapacity];
-    static uint16_t sampleBufferRed[sampleCapacity];
     int sampleIndex = 0;
     while (true) {
         uint16_t ir, red;
@@ -110,9 +115,17 @@ _Noreturn void App_Run(void) {
                 clearGraph();
                 drawGraph(sampleBufferIr, LCD_COLOR_LIGHTCYAN);
                 drawGraph(sampleBufferRed, LCD_COLOR_LIGHTRED);
+                performFFT();
+                drawGraph(fftBufferIr, LCD_COLOR_ORANGE);
                 sampleIndex = 0;
                 sensor.resetFifo();
             }
         }
     }
+}
+
+void performFFT() {
+    arm_rfft_instance_q15 fft_inst_q15;
+    arm_rfft_init_q15(&fft_inst_q15, 512, 0, 0);
+    arm_rfft_q15(&fft_inst_q15, (q15_t *) sampleBufferIr, (q15_t *) fftBufferIr);
 }
